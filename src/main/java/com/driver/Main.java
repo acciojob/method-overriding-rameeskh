3037c653cdbf4311230b9162e23500104f7d25d4
RAMEES K H <khramees.ramees@gmail.com>
2022-10-05 12:40:55 +0530

    first

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    A a=new A();
+    String s1=a.meth();
+    System.out.println(s1);
+
+    B b1=new B();
+    String s2=b1.meth();
+    System.out.println(s2);
+  }
+}
+class A{
+    public String meth(){
+        return "Invoking method from class A.";
+
+    }
+}
+class B extends A{
+    public String meth(){
+        return "Method is overridden in Extendend class B.";
+    }
 }
